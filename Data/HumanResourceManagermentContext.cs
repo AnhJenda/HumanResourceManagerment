5cf4b4abc696c19ca083fa42146907774da6694b
AnhJenda <quanganh.nqc99@gmail.com>
2023-03-09 10:32:03 +0700

    Initial commit

---
+++ Data/HumanResourceManagermentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HumanResourceManagerment;
+
+namespace HumanResourceManagerment.Data
+{
+    public class HumanResourceManagermentContext : DbContext
+    {
+        public HumanResourceManagermentContext (DbContextOptions<HumanResourceManagermentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HumanResourceManagerment.Employee> Employee { get; set; } = default!;
+    }
+}
---
+++ Employee.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HumanResourceManagerment
+{
+    [PrimaryKey("Id")]
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public double salary { get; set; }
+    }
+}
